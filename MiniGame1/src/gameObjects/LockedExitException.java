dcaf2570005635f9548bc3a11c404e78f51ad11c
Michael Conner <mconner3@ggc.edu>
2022-02-06 21:41:48 -0500

    Serial # for LockedExitException

--- MiniGame1/src/gameObjects/LockedExitException.java
+++ MiniGame1/src/gameObjects/LockedExitException.java
@@ -2,6 +2,11 @@ package gameObjects;
 
 public class LockedExitException extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public LockedExitException(String message) {
 		super(message);
 	}
